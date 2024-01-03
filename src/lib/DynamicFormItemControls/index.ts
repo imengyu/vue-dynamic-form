53e91eb0534acccff62135d14ca426f935ced8ea
imengyu <1501076885@qq.com>
2024-01-03 15:27:13 +0800

    增加缺少的导出类

--- src/lib/DynamicFormItemControls/index.ts
+++ src/lib/DynamicFormItemControls/index.ts
@@ -3,3 +3,6 @@ export * from './BaseSelect';
 export * from './BaseCheck';
 export * from './BaseTextArea';
 export * from './BaseInput';
+export * from './BaseDivider';
+export * from './FormArrayGroup';
+export * from './FormGroup';
